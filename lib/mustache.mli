681d8634386d8603514307230ff2c091b00be13b
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-26 15:48:13 -0800

    made Mustache.t abstract

--- lib/mustache.mli
+++ lib/mustache.mli
@@ -1,18 +1,7 @@
 exception Invalid_param of string with sexp
 exception Invalid_template of string with sexp
 
-type t =
-  | Iter_var
-  | String of string
-  | Escaped of string                
-  | Section of section
-  | Unescaped of string
-  | Partial of string
-  | Concat of t list
-and section = {
-  name: string;
-  contents: t;
-} with sexp
+type t with sexp
 
 val of_string : string -> t
 val to_string : t -> string
