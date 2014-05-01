382e96e7351dea40539287786d928a9c2659001d
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-04-30 23:03:05 -0400

    improve ocamldoc

--- lib/mustache.mli
+++ lib/mustache.mli
@@ -1,3 +1,4 @@
+(** A module for creating and rendering mustache templates in OCaml *)
 exception Invalid_param of string with sexp
 exception Invalid_template of string with sexp
 
@@ -7,9 +8,9 @@ val of_string : string -> t
 val to_string : t -> string
 val render : t -> Ezjsonm.t -> string
 
-val concat : t list -> t
 (** Concatenate a list of mustache templates. Faster than concatenating
     them as strings. *)
+val concat : t list -> t
 
 module Infix : sig
   val (^) : t -> t -> t
@@ -17,6 +18,6 @@ end
 
 val escape_html : string -> string
 
+(** Exported only for testing. Do not use *)
 val tokenize : string -> ('a * string) list
   -> [> `Text of string | `Token of 'a * string ] list
-(** Exported only for testing. Do not use *)
