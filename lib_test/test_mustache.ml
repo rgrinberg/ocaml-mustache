e20e9f2cde0d764c41a10beba43b3716d41c160e
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-11-13 23:37:32 -0500

    update tests

--- lib_test/test_mustache.ml
+++ lib_test/test_mustache.ml
@@ -8,36 +8,19 @@ module String = StringLabels
 let list_string_printer s =
   "(" ^ (String.concat ~sep:";" s) ^ ")"
 
-let token1 _ =
-  let s = "  rudi  __ testing _ 123 __ kthulu _ borg_453" in
-  let (words, numbers, text) = (ref [], ref [], ref 0) in
-  let (e_words, e_numbers, e_text) = (
-    ["rudi"; "testing"; "kthulu"; "borg"],
-    ["123"; "453"],
-    5
-  ) in
-  let tokens = Mustache.tokenize s [
-    (`Word, " *\\([a-z]+\\) *");
-    (`Number, " *\\([0-9]+\\) *");
-  ] in
-  tokens |> List.iter ~f:(function
-    | `Text s -> incr text
-    | `Token (`Word, s) -> words := s :: (!words)
-    | `Token (`Number, s) -> numbers := s :: (!numbers)
-  );
-  let printer = list_string_printer in
-  assert_equal ~printer ~msg:"words" !words (List.rev e_words);
-  assert_equal ~printer ~msg:"numbers" !numbers (List.rev e_numbers);
-  assert_equal ~msg:"text" ~printer:string_of_int !text e_text
+let mustache_printer t =
+  t |> Mustache.sexp_of_t |> Sexplib.Sexp.to_string_hum
 
+let string_printer x = x
 
 let mustache1 _ =
   let s = "Hello {{ name }}!" in
   let tmpl = Mustache.of_string s in
+  printf "Hello tmplate: %s\n" (mustache_printer tmpl);
   (* printf "tmpl parsed: %s\n" (Mustache.to_string tmpl); *)
   let js = `O ["name", `String "testing"] in
   (* printf "Rendered: %s\n" (Mustache.render tmpl js); *)
-  assert_equal "Hello testing!" (Mustache.render tmpl js)
+  assert_equal ~printer:string_printer "Hello testing!" (Mustache.render tmpl js)
 
 let mustache2 _ =
   let tmpl = Mustache.of_string "{{#bool}}there{{/bool}}" in
@@ -87,7 +70,6 @@ let test_sexp_conversion _ =
 let suite =
   "test mustache" >:::
   [
-    "test simple token parsing" >:: token1;
     "mustache1" >:: mustache1;
     "bool sections" >:: mustache2;
     "mustache section list 1" >:: mustache_section_list1;
