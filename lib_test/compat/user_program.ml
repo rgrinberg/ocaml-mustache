0066e66c97c90e60e453e2aec8b2e5f2267c5910
Gabriel Scherer <gabriel.scherer@gmail.com>
2021-01-05 15:56:45 +0100

    A compatibility test against the ocaml-mustache v2.0.0 API
    This test is designed to get a sense of the backward-compatibility
    impact of changes to the ocaml-mustache library.
    
    mustache_v200.mli is exactly a copy of the Mustache interface as it
    existed in the version v2.0.0, and it should not change.
    
    mustache_v200.ml is a reimplementation of this interface using the
    current ocaml-mustache library. If the library changes, this
    reimplementation will have to be fixed as well; the invasiveness of
    this fix can be used to estimate the invasiveness of the change for
    end-users.
    
    user_program.ml is written against this interface, and we check that
    it keeps working.
    
    v2.0.0 was chosen because it does not contain an explicit AST
    definition or "fold" function -- whose compatibility breaks for
    basically any new language feature or representation
    change (for example when adding locations in the AST).

---
+++ lib_test/compat/user_program.ml
@@ -0,0 +1,66 @@
+module Mustache = Mustache_v200
+(* A simple user program that could have been written against ocaml-mustache 2.0.0;
+   we check that it keeps working as expected. *)
+
+let json =
+  `O [ "name", `String "OCaml"
+     ; "qualities", `A [ `O ["name", `String "awesome"]
+                       ; `O ["name", `String "simple"]
+                       ; `O ["name", `String "fun"]
+                       ]
+     ]
+
+let section heading =
+  print_newline ();
+  print_string "# "; print_endline heading
+let subsection heading =
+  print_newline ();
+  print_string "## "; print_endline heading
+
+let test tmpl =
+  subsection "parsed";
+  print_endline "---";
+  print_endline (Mustache.to_string tmpl);
+  print_endline "---";
+
+  subsection "rendered";
+  print_endline "---";
+  print_endline (Mustache.render tmpl json);
+  print_endline "---";
+  ()
+  
+
+let () = section "Parsed template"
+
+let parsed_template =
+  Mustache.of_string "Hello {{name}}\n\
+                      Mustache is:\n\
+                      {{#qualities}}\n\
+                      * {{name}}\n\
+                      {{/qualities}}\n"
+
+let () = test parsed_template
+
+let () = section "Programmed template"
+
+let programmed_template =
+  let open Mustache in
+  concat [
+    raw "Hello "; escaped "name"; raw "\n";
+    raw "Mustache is:"; raw "\n";
+    section "qualities" @@ concat [
+      raw "* "; escaped "name"; raw "\n";
+    ]
+  ]
+
+let () = test programmed_template
+
+
+let () = section "Output comparison"
+let () =
+  let parsed_output = Mustache.render parsed_template json in
+  let programmed_output = Mustache.render programmed_template json in
+  if String.equal parsed_output programmed_output then
+    print_endline "Outputs match as expected."
+  else 
+    print_endline "Outputs DO NOT match, this is suspcious."
